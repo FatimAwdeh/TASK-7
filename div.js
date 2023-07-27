33cd949b579b1f481eeac87d96bcb5a71cbc6310
Fatima Awdeh <fatimaawdeh21@gmai.com>
2023-07-27 10:54:47 +0300

    Done with TASK-7

--- div.js
+++ div.js
@@ -1,12 +1,12 @@
 for(let n=0; n <=20 ;n++ ){
-    if(n % 3==0){
-        console.log(n + " " +"Divi par 3");
+    if(n % 3==0 && n % 5==0){
+        console.log(n + " " +"Divi par 3 et 5");
     }
     else if(n % 5 ==0){
         console.log(n +"  "+"Divi par 5");
     }
-    else if(n % 3==0 && n % 5==0){
-        console.log(n+"  "+"Divi par 5 et 3");
+    else if(n % 3==0){
+        console.log(n+"  "+"Divi par  3");
     }
     else{
         console.log(n);
